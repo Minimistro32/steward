afe7c4de64af3cde3e53df3ebc6e138b1e119d80
Minimistro32 <tyson.freeze@icloud.com>
2026-08-06 22:56:03 +0000

    add todo

--- frontend/steward-ui/src/lib/models/policies/OverridePolicy.ts
+++ frontend/steward-ui/src/lib/models/policies/OverridePolicy.ts
@@ -1,5 +1,9 @@
 import type { Allowance } from "..";
 
+// TODO: Remove override policy completely. Allowed will completely depend on whether an OverrideAllowance is provided.
+// Move optional requirement and allowance (which should also become optional and renamed to OverrideAccess) to the policy directly.
+// A null `OverrideRequirement` means that nothing is required to submit an override request.
+
 export interface OverridePolicy {
     allowed: boolean;
     requirement?: OverrideRequirement;
